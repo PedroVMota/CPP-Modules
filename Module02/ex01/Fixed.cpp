933a49c488ad46d1981749ae5462e40be5416715
PedroVMota <pvital-m@student.42lisboa.com>
2024-02-10 13:23:01 +0000

    ex 03

--- Module02/ex01/Fixed.cpp
+++ Module02/ex01/Fixed.cpp
@@ -3,29 +3,22 @@
 
 Fixed::Fixed() : _rawBi(0)
 {
-    std::cout << "Default constructor called" << std::endl;
 }
 
 
-/// @brief Copy Constructor
-/// @param t 
 Fixed::Fixed(const Fixed &t): _rawBi(t._rawBi){
-    std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float &t) : _rawBi(roundf(t * (1 << this->_bitShift)))
 {
-    std::cout << "Fixed Float Constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int &t) : _rawBi(t << _bitShift)
 {
-    std::cout << "Fixed Float Constructor called" << std::endl;
 }
 
 
 Fixed &Fixed::operator=(const Fixed &other) {
-    std::cout << "Copy assignment operator called" << std::endl;
     if(this != &other)
         _rawBi = other._rawBi;
     return *this;
@@ -33,7 +26,6 @@ Fixed &Fixed::operator=(const Fixed &other) {
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
 }
 
 void	Fixed::setRawBits( int const raw ) {
@@ -41,7 +33,6 @@ void	Fixed::setRawBits( int const raw ) {
 }
 
 int		Fixed::getRawBits( void ) {
-	std::cout << "getRawBits member function called\n";
 	return ( _rawBi );
 }
 
@@ -52,6 +43,8 @@ float 	Fixed::toFloat( void ) const {
 int 	Fixed::toInt( void ) const {
 	return ( _rawBi >> _bitShift );
 }
+
+
 
 std::ostream& operator<<( std::ostream& os, const Fixed& fixed ) {
 	os << fixed.toFloat();
