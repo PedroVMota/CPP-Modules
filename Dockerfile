c51684a6fbddd4360a213ffa81dfae7bd12cc460
Pedro Miguel Vital Mota <pvital-m@c2r1s7.42lisboa.com>
2024-03-08 15:24:15 +0000

    Module 05

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM arm64v8/ubuntu
-
-#Setup valgrind ENVIROMENT
-RUN apt-get update && apt-get install -y curl cmake git gcc g++ valgrind
-
-
-
---
+++ Module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+class Bureaucrat{
+	private:
+		const std::string name;
+		int grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+
+};
